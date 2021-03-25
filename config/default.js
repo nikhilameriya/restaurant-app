cc5849152127fc06fc7240721b5b6dc80994cea0
Nikhil <nikhil@Subhashs-MacBook-Pro.local>
2021-03-25 16:26:58 +0530

    setup package json with initial app.js file

---
+++ config/default.js
@@ -0,0 +1,7 @@
+
+const Config = {};
+
+//We can setup multiple key : value with configs
+Config.baseURL = "https://api.yelp.com/v3/businesses/";
+Config.authToken = "Bearer C6dyWlVsqHH1QVPUIG3tyHHyRimYF2KIHBnfl5xSy_975sNxdfejdCrZVJGDoRQyfvfWC3oTCMydO2JY1YLd87dS0KETvNRJErhq_WM3hmU_qnkj6bEWeuHV7CvEX3Yx";
+module.exports = Config; // Utilizing module.exports because of build process.
